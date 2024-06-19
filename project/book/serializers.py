52410b12f4a044f365dbc1c412b750a6f6dd8e6d
Niloz23 <147528969+Niloz23@users.noreply.github.com>
2024-06-19 12:47:57 +0300

    Add files via upload

---
+++ project/book/serializers.py
@@ -0,0 +1,7 @@
+from .models import *
+from rest_framework import serializers
+from drf_writable_nested import WritableNestedModelSerializer
+
+class BoookSerializer(WritableNestedModelSerializer):
+
+
